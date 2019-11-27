74478f54168faf8fc81c260b19e32385af69c787
taufik66645 <taufik.66645@gmail.com>
2019-11-27 14:00:58 +0700

    First initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const port = 3002;
+const bodyParser = require('body-parser');
+
+const todoList = [
+    {
+        id: 1,
+        task: 'learn Express',
+        done: false
+    },
+    {
+        id: 2,
+        task: 'learn express generator',
+        done: false
+    }
+];
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=> {
+    res.send(todoList)
+});
+
+app.get('/:id', (req, res) => {
+    try{
+        const filteredTodo = todoList.find(item.id == req.params.id);
+        res.send({
+            message: 'here is what you looking for',
+            filteredTodo
+        });
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+app.post ('/', (req, res) => {
+    try {
+        let newId = todoList.length + 1;
+        let newTodo = {
+            id: newId,
+            task: req.body.task,
+            done: false
+        };
+
+        todoList.push(newTodo)
+
+        res.status(200).send({
+            message: 'todo sucessfully added',
+            todoList
+        });
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+app.delete('/:id', (req, res) => {
+    try {
+      const idToDelete = req.params.id;
+      let newTodo = todoList.filter(item => item.id !== parseInt(idToDelete));
+  
+      todoList = newTodo;
+  
+      res.status(200).send(todoList);
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
+  });
+  app.put('/:id', (req, res) => {
+    try {
+      let getTodoToUpdate = todoList.findIndex(data => data.id == req.params.id);
+  
+      todoList.map(data => {
+        if (data.id == req.params.id) {
+          todoList[getTodoToUpdate].task = req.body.task;
+        }
+      });
+      res.send({
+        message: 'data successfully updated',
+        todoList
+      });
+    } catch (error) {
+      res.send(error);
+    }
+  });
+  
+  app.listen(process.env.PORT, () => {
+    console.log(`Your server is running on PORT ${process.env.PORT}`)
+  });
+    
